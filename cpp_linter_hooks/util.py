ab2d6174283f8cb20560c936aed4c870bb87569c
shenxianpeng <xianpeng.shen@gmail.com>
2022-07-10 19:54:27 +0800

    update clang-tools install version

--- cpp_linter_hooks/util.py
+++ cpp_linter_hooks/util.py
@@ -2,17 +2,20 @@ import subprocess
 
 
 def check_installed(tool, version="") -> int:
-    if version == "":
-        command = [tool, '--version']
-    else:
+    if version:
         command = [f'{tool}-{version} ', '--version']
+    else:
+        command = [tool, '--version']
     try:
         subprocess.run(command, stdout=subprocess.PIPE)
         retval = 0
     except FileNotFoundError:
         # clang-tools exist because install_requires=['clang-tools'] in setup.py
         # install verison 13 by default if clang-tools not exist.
-        retval = subprocess.run(['clang-tools', '-i', '13'], stdout=subprocess.PIPE).returncode
+        if version:
+            retval = subprocess.run(['clang-tools', '-i', version], stdout=subprocess.PIPE).returncode
+        else:
+            retval = subprocess.run(['clang-tools', '-i', '13'], stdout=subprocess.PIPE).returncode
     return retval
 
 
