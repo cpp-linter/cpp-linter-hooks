a700f11b61a266ad03da0b66ad0c060535effd96
Peter Shen <xianpeng.shen@gmail.com>
2022-08-14 10:58:40 +0800

    feat: Add pyproject.toml (#4)

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
-from setuptools import find_packages
-from setuptools import setup
+#!/usr/bin/env python
+"""Info used for installing the cpp-linter-hooks package.
+Since using setup.py is no longer std convention,
+all install information is located in pyproject.toml
+"""
 
-setup(
-    name='cpp_linter_hooks',
-    description='Automatically check c/c++ with clang-format and clang-tidy',
-    url='https://github.com/cpp-linter/cpp-linter-hooks',
-    version='0.2.1',
-    author="Peter Shen",
-    author_email="xianpeng.shen@gmail.com",
-    license="MIT",
-    keywords="clang clang-tidy clang-format pre-commit pre-commit-hooks",
-    packages=find_packages(),
-    install_requires=['clang-tools>=0.2.2'],
-    entry_points={
-        "console_scripts": [
-            "clang-format-hook=cpp_linter_hooks.clang_format:main",
-            "clang-tidy-hook=cpp_linter_hooks.clang_tidy:main",
-        ]
-    },
-)
+import setuptools
+
+
+setuptools.setup()
