6a00c8a951ca3373e394b2103a1baf6f1a28d6da
shenxianpeng <xianpeng.shen@gmail.com>
2024-08-16 17:25:54 +0300

    remove tmp_path

--- tests/test_clang_tidy.py
+++ tests/test_clang_tidy.py
@@ -19,10 +19,10 @@ def generate_compilation_database():
         (['--checks="boost-*"', '--version=16'], 1),
     ),
 )
-def test_run_clang_tidy_valid(args, expected_retval, tmp_path, generate_compilation_database):
+def test_run_clang_tidy_valid(args, expected_retval, generate_compilation_database):
     # copy test file to tmp_path to prevent modifying repo data
     generate_compilation_database
-    test_file = tmp_path / "main.c"
+    test_file = Path("testing/main.c")
     test_file.write_bytes(Path("testing/main.c").read_bytes())
     ret, output = run_clang_tidy(args + [str(test_file)])
     assert ret == expected_retval
