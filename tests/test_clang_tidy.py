46d7d71f84836a77551e771829190ff7d99f008f
shenxianpeng <xianpeng.shen@gmail.com>
2025-07-01 22:42:55 +0300

    feat: add tests and format code using `ruff` (#75)
    * test: test dry-run
    
    * fix lint fail
    
    * fix test failure
    
    * Replace the unused local variable output with _
    
    * use ruff and format code

--- tests/test_clang_tidy.py
+++ tests/test_clang_tidy.py
@@ -5,21 +5,22 @@ from pathlib import Path
 from cpp_linter_hooks.clang_tidy import run_clang_tidy
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def generate_compilation_database():
-    subprocess.run(['mkdir', '-p', 'build'])
-    subprocess.run(['cmake', '-Bbuild', 'testing/'])
-    subprocess.run(['cmake', '-Bbuild', 'testing/'])
+    subprocess.run(["mkdir", "-p", "build"])
+    subprocess.run(["cmake", "-Bbuild", "testing/"])
+    subprocess.run(["cmake", "-Bbuild", "testing/"])
 
 
 @pytest.mark.parametrize(
-    ('args', 'expected_retval'), (
+    ("args", "expected_retval"),
+    (
         (['--checks="boost-*"'], 1),
-        (['--checks="boost-*"', '--version=16'], 1),
-        (['--checks="boost-*"', '--version=17'], 1),
-        (['--checks="boost-*"', '--version=18'], 1),
-        (['--checks="boost-*"', '--version=19'], 1),
-        (['--checks="boost-*"', '--version=20'], 1),
+        (['--checks="boost-*"', "--version=16"], 1),
+        (['--checks="boost-*"', "--version=17"], 1),
+        (['--checks="boost-*"', "--version=18"], 1),
+        (['--checks="boost-*"', "--version=19"], 1),
+        (['--checks="boost-*"', "--version=20"], 1),
     ),
 )
 def test_run_clang_tidy_valid(args, expected_retval):
@@ -32,13 +33,14 @@ def test_run_clang_tidy_valid(args, expected_retval):
 
 
 @pytest.mark.parametrize(
-    ('args', 'expected_retval'), (
+    ("args", "expected_retval"),
+    (
         (['--checks="boost-*"'], 1),
-        (['--checks="boost-*"', '--version=16'], 1),
-        (['--checks="boost-*"', '--version=17'], 1),
-        (['--checks="boost-*"', '--version=18'], 1),
-        (['--checks="boost-*"', '--version=19'], 1),
-        (['--checks="boost-*"', '--version=20'], 1),
+        (['--checks="boost-*"', "--version=16"], 1),
+        (['--checks="boost-*"', "--version=17"], 1),
+        (['--checks="boost-*"', "--version=18"], 1),
+        (['--checks="boost-*"', "--version=19"], 1),
+        (['--checks="boost-*"', "--version=20"], 1),
     ),
 )
 def test_run_clang_tidy_invalid(args, expected_retval, tmp_path):
