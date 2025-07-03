7368f3ba52f9c9e8b36196710011f9c16dd6ba64
shenxianpeng <xianpeng.shen@gmail.com>
2025-07-04 02:41:16 +0300

    update testing run.sh

--- testing/run.sh
+++ testing/run.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 # 2 Failed cases are expected
+echo "==========================="
+echo "Test pre-commit-config.yaml"
+echo "==========================="
 pre-commit clean
 pre-commit run -c testing/pre-commit-config.yaml --files testing/main.c | tee -a result.txt || true
 git restore testing/main.c
 
 # 10 Failed cases are expected
+echo "===================================="
+echo "Test pre-commit-config-version.yaml"
+echo "===================================="
 pre-commit clean
 pre-commit run -c testing/pre-commit-config-version.yaml --files testing/main.c | tee -a result.txt || true
 git restore testing/main.c
-cat result.txt
 
 # 2 Failed cases are expected
+echo "===================================="
+echo "Test pre-commit-config-verbose.yaml"
+echo "===================================="
 pre-commit clean
 pre-commit run -c testing/pre-commit-config-verbose.yaml --files testing/main.c | tee -a result.txt || true
 git restore testing/main.c
+
+echo "===================================="
+echo "print result.txt"
 cat result.txt
+echo "===================================="
 
 failed_cases=`grep -c "Failed" result.txt`
 
@@ -24,8 +36,8 @@ if [ $failed_cases -eq 10 ]; then
     echo "=============================="
     echo "Test cpp-linter-hooks success."
     echo "=============================="
-    exit 0
     rm result.txt
+    exit 0
 else
     echo "============================="
     echo "Test cpp-linter-hooks failed."
