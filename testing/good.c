f7107c48ec8c895342d5d064a2f5928622bcbed5
shenxianpeng <xianpeng.shen@gmail.com>
2025-09-19 23:04:14 +0300

    fix: revert example code

--- testing/good.c
+++ testing/good.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-int main()
-{
-    for (;;)
-        break;
-    printf("Hello world!\n");
-    return 0;
+int main() {
+  for (;;) break;
+  printf("Hello world!\n");
+  return 0;
 }
