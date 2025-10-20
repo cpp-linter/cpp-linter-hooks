c31b410465546e04b6920f395451198c9ceda82d
shenxianpeng <xianpeng.shen@gmail.com>
2025-10-20 22:44:52 +0300

    feat: create script to update versions automaticlly

--- cpp_linter_hooks/util.py
+++ cpp_linter_hooks/util.py
@@ -10,6 +10,8 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
+from cpp_linter_hooks.versions import CLANG_FORMAT_VERSIONS, CLANG_TIDY_VERSIONS
+
 LOG = logging.getLogger(__name__)
 
 
@@ -30,95 +32,6 @@ def get_version_from_dependency(tool: str) -> Optional[str]:
 
 DEFAULT_CLANG_FORMAT_VERSION = get_version_from_dependency("clang-format")
 DEFAULT_CLANG_TIDY_VERSION = get_version_from_dependency("clang-tidy")
-
-
-# https://pypi.org/pypi/clang-format/json
-CLANG_FORMAT_VERSIONS = [
-    "6.0.1",
-    "7.1.0",
-    "8.0.1",
-    "9.0.0",
-    "10.0.1",
-    "10.0.1.1",
-    "11.0.1",
-    "11.0.1.1",
-    "11.0.1.2",
-    "11.1.0",
-    "11.1.0.1",
-    "11.1.0.2",
-    "12.0.1",
-    "12.0.1.1",
-    "12.0.1.2",
-    "13.0.0",
-    "13.0.1",
-    "13.0.1.1",
-    "14.0.0",
-    "14.0.1",
-    "14.0.3",
-    "14.0.4",
-    "14.0.5",
-    "14.0.6",
-    "15.0.4",
-    "15.0.6",
-    "15.0.7",
-    "16.0.0",
-    "16.0.1",
-    "16.0.2",
-    "16.0.3",
-    "16.0.4",
-    "16.0.5",
-    "16.0.6",
-    "17.0.1",
-    "17.0.2",
-    "17.0.3",
-    "17.0.4",
-    "17.0.5",
-    "17.0.6",
-    "18.1.0",
-    "18.1.1",
-    "18.1.2",
-    "18.1.3",
-    "18.1.4",
-    "18.1.5",
-    "18.1.6",
-    "18.1.7",
-    "18.1.8",
-    "19.1.0",
-    "19.1.1",
-    "19.1.2",
-    "19.1.3",
-    "19.1.4",
-    "19.1.5",
-    "19.1.6",
-    "19.1.7",
-    "20.1.0",
-    "20.1.3",
-    "20.1.4",
-    "20.1.5",
-    "20.1.6",
-    "20.1.7",
-    "20.1.8",
-    "21.1.0",
-    "21.1.1",
-    "21.1.2",
-]
-
-# https://pypi.org/pypi/clang-tidy/json
-CLANG_TIDY_VERSIONS = [
-    "13.0.1.1",
-    "14.0.6",
-    "15.0.2",
-    "15.0.2.1",
-    "16.0.4",
-    "17.0.1",
-    "18.1.1",
-    "18.1.8",
-    "19.1.0",
-    "19.1.0.1",
-    "20.1.0",
-    "21.1.0",
-    "21.1.1",
-]
 
 
 def _resolve_version(versions: List[str], user_input: Optional[str]) -> Optional[str]:
